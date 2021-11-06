5962958a13be3ff0393400d1195b390cdb80e412
Oksana <79263143446@mail.ru>
2021-11-06 17:20:43 +0300

    task04

---
+++ task04/Program.cs
@@ -0,0 +1,15 @@
+﻿// По введенному номеру дня недели вывести его название
+
+
+Console.WriteLine("Введите цифру от 1 до 7");
+int a=int.Parse(Console.ReadLine());
+
+   
+    if (a==1) {Console.WriteLine("Monday");}
+    if (a==2) {Console.WriteLine("Tuesday");}
+    if (a==3) {Console.WriteLine("Wednesday");}
+    if (a==4) {Console.WriteLine("Thursday");}
+    if (a==5) {Console.WriteLine("Friday");}
+    if (a==6) {Console.WriteLine("Saturday");}
+    if (a==7) {Console.WriteLine("Sunday");}
+    
