1f7262902e5ebfa990bffc5f4ed70056c4058d5b
Oksana <79263143446@mail.ru>
2021-11-06 14:42:13 +0300

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int a=55;
+int b=9;
+if (a>b)
+{
+    int max=a;
+    int min=b;
+    Console.WriteLine($"Max = {max}  Min = {min}");
+}
+else
+{
+    int max=b;
+    int min=a;
+    Console.WriteLine($"Max = {max}  Min = {min}"); 
+}
