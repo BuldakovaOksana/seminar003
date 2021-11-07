0a165faf002c59d59904d9db55e638fb630f812f
Oksana <79263143446@mail.ru>
2021-11-07 22:50:48 +0300

    task05

---
+++ task05/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти максимальное из трех чисел
+
+
+int [] array= new int[3];
+for (int i = 0; i < array.Length; i++)
+{
+ array[i]=new Random().Next(1,10);
+ Console.Write($"{array[i]} ");   
+}
+Console.WriteLine();
+
+int max=array[0];
+for (int i = 0; i < array.Length; i++)
+{
+if (max<array[i])
+{
+    max=array[i];
+    
+}
+}
+
+ Console.WriteLine(max);   
