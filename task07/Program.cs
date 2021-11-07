c6e92fb5bccf8cc9237aadebf8758927be54767a
Oksana <79263143446@mail.ru>
2021-11-07 23:30:39 +0300

    task07

---
+++ task07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным 
+
+void function(int a)
+{
+    if (a%2==0)
+    {
+        Console.WriteLine("Четное");
+    }
+    else
+    {
+        Console.WriteLine("Нечетное");
+    }
+}
+function(9);
