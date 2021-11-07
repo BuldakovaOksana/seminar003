5bba29da930ece7528e50c220316faffa81d969d
Oksana <79263143446@mail.ru>
2021-11-07 23:06:09 +0300

    task06

---
+++ task06/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления значения функции y=f(a)
+
+double function(double a)
+{
+   double y=a*a+1;
+   return y;
+}
+double result=function(7);
+Console.WriteLine(result);
