bea340dbe12b9580d4e12d5121b35a6aa91ad109
Oksana <79263143446@mail.ru>
2021-11-09 13:43:46 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+System.Console.WriteLine("Введите трехзначное число");
+int a= int.Parse(Console.ReadLine());
+
+int lastDigit(int a)
+{
+    int result= a%10;
+    return result;
+}
+int b=lastDigit(a);
+System.Console.WriteLine(b);
