c682c3b324c331e6c1161ecfe3046697ffe96233
Oksana <79263143446@mail.ru>
2021-11-10 00:01:16 +0300

    task12

--- task12/Program.cs
+++ task12/Program.cs
@@ -1,23 +1,36 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 
 
-int number = new Random().Next(10, 100);
-//System.Console.WriteLine(number);
+// int number = new Random().Next(10, 100);
+// //System.Console.WriteLine(number);
 
-int findMax(int number)
+// int findMax(int number)
+// {
+//     int a = number / 10;
+//     int b = number % 10;
+//     int max=0;
+//     if (a > b)
+//     {
+//         max = a;
+//     }
+//     else
+//     {
+//         max = b;
+//     }
+//     return max;
+// }
+// int result=findMax(number);
+// System.Console.WriteLine($"{result} наибольшая цифра числа {number}");
+
+
+char GetMaxNumber(int num)
 {
-    int a = number / 10;
-    int b = number % 10;
-    int max=0;
-    if (a > b)
-    {
-        max = a;
-    }
-    else
-    {
-        max = b;
-    }
-    return max;
+    string ab = Convert.ToString(num);
+    char a = ab[0];
+    char b = ab[1];
+    if (a > b) return a;
+    else return b;
 }
-int result=findMax(number);
-System.Console.WriteLine($"{result} наибольшая цифра числа {number}");
+int G = new Random().Next(10, 100);
+char result = GetMaxNumber(G);
+Console.WriteLine($"В числе {G} наибольшая цифра - {result}");
