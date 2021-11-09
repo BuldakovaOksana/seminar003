2e694e58ac04f09f499605b81ae2c3c8b0e536d6
Oksana <79263143446@mail.ru>
2021-11-09 12:18:43 +0300

    task09

---
+++ task09/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать четные числа от 1 до N
+
+
+int[]array=new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array [i]=new Random().Next(1,100);
+    
+    System.Console.Write($"{array[i]} ");
+}
+ System.Console.WriteLine();
+
+ for (int i = 0; i < array.Length; i++)
+ {
+     if (array[i]%2==0)
+     {
+         System.Console.Write($"{array[i]} ");
+     }
+ }
