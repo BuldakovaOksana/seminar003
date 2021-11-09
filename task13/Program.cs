1d6fe882bae8e26288a354c9579ad13fd66497ae
Oksana <79263143446@mail.ru>
2021-11-09 22:12:50 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+
+int a = new Random().Next(100, 1000);
+System.Console.WriteLine(a);
+
+int deleteDigit(int a)
+{
+    int b = a / 100 * 10 + a % 10;
+    return b;
+}
+int result = deleteDigit(a);
+System.Console.WriteLine(result);
