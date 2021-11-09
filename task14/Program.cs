6634bf638c4775d9178cd90de8a2801f59264a4e
Oksana <79263143446@mail.ru>
2021-11-09 22:57:17 +0300

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,25 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+
+int numberFirst=new Random().Next(100,1000);
+int numberSecond=new Random().Next(1,10);
+
+System.Console.WriteLine($"Делимое {numberFirst}");
+System.Console.WriteLine($"Делитель {numberSecond}");
+
+int withdrawRemainder( int numberFirst, int numberSecond)
+{
+if (numberFirst%numberSecond==0) 
+{
+    int b=0;
+System.Console.WriteLine("Кратно");
+return b;
+}
+else 
+{
+    int a= numberFirst%numberSecond;
+    return a;
+}
+}
+int result=withdrawRemainder ( numberFirst, numberSecond);
+System.Console.WriteLine($"Остаток {result}");
