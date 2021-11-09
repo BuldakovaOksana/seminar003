aeac70f2792c71570bcf33b1ca4577cdbf27d527
Oksana <79263143446@mail.ru>
2021-11-09 14:25:37 +0300

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать вторую цифру трёхзначного числа
+System.Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine());
+
+int secondDigit(int a)
+{
+    int result = a / 10 % 10;
+    return result;
+}
+int b = secondDigit(a);
+System.Console.WriteLine($"Второе число {b}");
