9d955c7a940558f0252d9a37afbc0e7eb97fb9dc
Oksana <79263143446@mail.ru>
2021-11-10 00:00:20 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти третью цифру числа или сообщить, что её нет*/ 
+
+
+int number = new Random().Next(1, 100);
+System.Console.WriteLine(number);
+
+if (number < 100)
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    int a = number / 100 % 10;
+    System.Console.WriteLine($"Третья цифра {a}");
+}
+
+
+
+
