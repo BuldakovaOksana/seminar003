79fe4c73add0aa08ae3630d9a8fcf5d9711842dd
Oksana <79263143446@mail.ru>
2021-11-10 11:39:06 +0300

    task16

---
+++ task16/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Дано число. Проверить кратно ли оно 7 и 23
+
+
+int number = new Random().Next(1, 1000);
+System.Console.WriteLine(number);
+int a = 7;
+int b = 23;
+
+void multiplicity(int number, int a, int b)
+{
+    if (number % a == 0 && number % b == 0)
+    {
+        System.Console.WriteLine("Кратно");
+    }
+    else
+    {
+        System.Console.WriteLine("Не кратно");
+    }
+}
+multiplicity(number, a, b);
