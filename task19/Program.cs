e9fda2b1c95aac9e75d863702c33454d7d9b6f09
Oksana <79263143446@mail.ru>
2021-11-11 10:27:40 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+ bool x=true;
+bool y=false;
+if   (!(x|y)==!x&!y)
+{
+    System.Console.WriteLine(true);
+}
+else
+{
+    System.Console.WriteLine(false);
+}
