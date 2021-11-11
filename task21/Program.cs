c8f11a813ad4b0d3423c1f28a70f0f23c3f2d4b4
Oksana <79263143446@mail.ru>
2021-11-11 14:52:45 +0300

    task21

---
+++ task21/Program.cs
@@ -0,0 +1,34 @@
+﻿// Ввести номер четверти, показать диапазоны для возможных координат
+
+
+
+System.Console.WriteLine("Введите номер четверти");
+int a = int.Parse(Console.ReadLine());
+
+void showRange(int a)
+{
+if (a > 0 && a < 5)
+{
+    if (a == 1)
+    {
+        System.Console.WriteLine("Диапазон: x>0 & y>0");
+    }
+    if (a == 2)
+    {
+        System.Console.WriteLine("Диапазон: x<0 & y>0");
+    }
+    if (a == 3)
+    {
+        System.Console.WriteLine("Диапазон: x<0 & y<0");
+    }
+    if (a == 4)
+    {
+        System.Console.WriteLine("Диапазон: x>0 & y<0");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введите цыфру от 1 до 4");
+}
+}
+showRange(a);
