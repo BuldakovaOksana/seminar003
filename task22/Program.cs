9f5f5fedf4749d478aa28cd57a7ae784c6deaf63
Oksana <79263143446@mail.ru>
2021-11-11 15:45:13 +0300

    task22

---
+++ task22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+
+
+int number=new Random().Next(10000,100000);
+
+int digitFirst= number/10000;
+int digitSecond=number/1000%10;
+int digitFourth=number/10%10;
+int digitFifth=number%10;
+System.Console.WriteLine($"{number} ");
+System.Console.Write($"{digitFirst} {digitSecond} {digitFourth} {digitFifth} ");
+
+if (digitFirst==digitFifth&&digitSecond==digitFourth)
+{
+    System.Console.WriteLine("Палиндромом");
+}
+else
+{
+    System.Console.WriteLine("Не палиндромом");
+}
