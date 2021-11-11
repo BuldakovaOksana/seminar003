1a840b64d26a603cd5f5a431ae9cf94d8bc02b8b
Oksana <79263143446@mail.ru>
2021-11-11 17:54:20 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+
+
+// int x1 = new Random().Next(1, 5);
+// int x2 = new Random().Next(5, 10);
+// int y1 = new Random().Next(1, 5);
+// int y2 = new Random().Next(5, 10);
+
+
+// int d = Convert.ToInt32(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+// System.Console.Write($"Расстояние между точками: {x1} {x2} {y1} {y2} в пространстве 2D ровняется {d} ");
+
+
