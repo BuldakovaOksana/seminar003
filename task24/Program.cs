75abbf16d08629574066b8517129b98768027cfe
Oksana <79263143446@mail.ru>
2021-11-15 16:24:41 +0300

    task25

--- task24/Program.cs
+++ task24/Program.cs
@@ -8,8 +8,8 @@ System.Console.WriteLine("Таблица квадратов");
 
 for (int i = 1; i <=10; i++)
 {
-    int a = i*i*i;
-    System.Console.WriteLine($"{i}x{i}x{i}={a}");
+    int a = i*i;
+    System.Console.WriteLine($"{i}x{i}={a}");
 }
 
 
---
+++ task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти кубы чисел от 1 до N
+
+
+
+
+
+System.Console.WriteLine("Таблица квадратов");
+
+
+for (int i = 1; i <=10; i++)
+{
+    int a = i*i*i;
+    System.Console.WriteLine($"{i}x{i}x{i}={a}");
+}
