b670828f8db5f2e6e12e0b8d38ad1713972ea37c
Oksana <79263143446@mail.ru>
2021-11-15 16:14:09 +0300

    task24

---
+++ task24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+
+
+
+System.Console.WriteLine("Таблица квадратов");
+
+
+for (int i = 1; i <=10; i++)
+{
+    int a = i*i*i;
+    System.Console.WriteLine($"{i}x{i}x{i}={a}");
+}
+
+
+// int Method()
+// {
+// for (int i = 1; i <=10; i++)
+// {
+//     int a = i*i*i;
+//     return ({i}x{i}x{i}={a});
+// }
+// }
+// int result= Method();
+// System.Console.WriteLine();
+
