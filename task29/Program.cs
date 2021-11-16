eb6efb20b24d9e1b564e4d710a5b32fecc42b8dd
Oksana <79263143446@mail.ru>
2021-11-16 15:27:45 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+
+
+int calculateAmount(int number)
+{
+int sum=0;
+while (number>0)
+{
+     sum=sum+number%10;
+     number=number/10;
+}
+return sum;
+}
+int result=calculateAmount(234);
+System.Console.WriteLine(result);
