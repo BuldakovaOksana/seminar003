0f78eecd9a3466f6eb542aaf211b15c5fe6d60db
Oksana <79263143446@mail.ru>
2021-11-16 13:34:01 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,16 @@
+﻿// Определить количество цифр в числе
+
+int determineQuantity(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+int result = determineQuantity(12345678);
+System.Console.WriteLine(result);
+
+
