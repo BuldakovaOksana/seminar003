6d4fb7c539fa8bbd91b3e3b4a078ae5b13a7055f
Oksana <79263143446@mail.ru>
2021-11-16 15:58:40 +0300

    task30

---
+++ task30/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+
+
+int productsNumbers(int N)
+{
+int product=1;
+for (int i = 1; i <= N; i++)
+{
+    product=product*i;
+}
+return product;
+}
+int result= productsNumbers(5);
+System.Console.WriteLine(result);
