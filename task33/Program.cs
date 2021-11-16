53686ae74197e574180847b2ae1bdcad7a3c3f7f
Oksana <79263143446@mail.ru>
2021-11-16 22:03:36 +0300

    task33

---
+++ task33/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+
+void PrintArray()
+{
+    int[]array=new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+     array[i]=new Random().Next(0,2);
+     System.Console.WriteLine(array[i]);   
+    }
+}
+PrintArray();
