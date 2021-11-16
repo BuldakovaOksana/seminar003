be0a9a79f8ce7d3723bd53617ca9f321120824b6
Oksana <79263143446@mail.ru>
2021-11-16 16:34:01 +0300

    task31

---
+++ task31/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+
+
+for (int i = 1; i < 10; i++)
+{
+    int cube =i*i*i;
+    if (cube%2==0)
+    {
+        System.Console.WriteLine(cube);
+    }
+}
