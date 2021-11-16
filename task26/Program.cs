bcb111848591a607990b01a741c492e9e33a5ca8
Oksana <79263143446@mail.ru>
2021-11-16 11:23:37 +0300

    task26

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,8 +1,23 @@
 ﻿// Найти сумму чисел от 1 до А
 
+// int a=0;
+// for (int i = 0; i < 10; i++)
+// {
+//     a=a+i;
+//     System.Console.WriteLine($"{i} {a}");
+// }
+
+
+
+int sum( int number)
+{
 int a=0;
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < number; i++)
 {
     a=a+i;
-    System.Console.WriteLine($"{i} {a}");
+    
 }
+return a;
+}
+int result=sum(10);
+System.Console.WriteLine(result);
