067330649e4f0fafbb020deed58b83140c9a57d0
Oksana <79263143446@mail.ru>
2021-11-16 12:00:31 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+
+int exponentiationNumber(int numberA, int power)
+{
+
+int variable=1;
+
+for (int i = 0; i < power; i++)
+{
+    variable=variable*numberA;
+}
+return variable;
+}
+int result=exponentiationNumber(5,5);
+System.Console.WriteLine(result);
