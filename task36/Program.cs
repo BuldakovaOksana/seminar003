7b8b7396ccb7bb769d06a2569d0c22c662543a7e
Oksana <79263143446@mail.ru>
2021-11-17 15:20:44 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число 
+
+
+int[] array = new int[10];
+
+int[] fillarray(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = new Random().Next(1, 10);
+    }
+    return A;
+}
+
+
+string printarray(int[] B)
+{
+    string res = string.Empty;
+    for (int i = 0; i < B.Length; i++)
+    {
+        res += $"{B[i]} ";
+    }
+    return res;
+}
+
+fillarray(array);
+string print = printarray(array);
+System.Console.WriteLine(print);
+System.Console.WriteLine();
+
+
+void identifyPresent(int[] C, int number)
+{
+    for (int i = 0; i < C.Length; i++)
+    {
+        if (C[i] == number)
+        {
+
+            System.Console.WriteLine($"Число присутствует под индексом № {i}");
+        }
+
+    }
+
+}
+
+identifyPresent(array, 7);
