d8088e07af2da5630c791dcea388ab61c0656bd5
Oksana <79263143446@mail.ru>
2021-11-17 23:57:41 +0300

    task40

---
+++ task40/Program.cs
@@ -0,0 +1,31 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+
+
+int[] array = new int[10];
+
+int[] FillArray(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = new Random().Next(1, 11);
+        System.Console.Write(A[i] + " ");
+    }
+    return A;
+}
+FillArray(array);
+
+System.Console.WriteLine();
+
+string productsPairs(int[] B)
+{
+    int j = B.Length - 1;
+    string result = string.Empty;
+    for (int i = 0; i < B.Length / 2; i++)
+    {
+        result += array[i] * array[B.Length - 1 - i] + " ";
+    }
+    return result;
+}
+string res = productsPairs(array);
+System.Console.WriteLine(res);
