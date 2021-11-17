a74ad4a7996a269904d3072d4eda2fb8e4c9318b
Oksana <79263143446@mail.ru>
2021-11-17 13:58:27 +0300

    task35

---
+++ task35/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+
+int[] array = new int[10];
+
+int[] fillarray(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = new Random().Next(-10, 11);
+    }
+    return A;
+}
+
+
+string printarray(int[] B)
+{
+    string print = string.Empty;
+    for (int i = 0; i < B.Length; i++)
+    {
+        print += $"{B[i]} ";
+    }
+    return print;
+}
+fillarray(array);
+string result = printarray(array);
+System.Console.WriteLine(result);
+System.Console.WriteLine();
+
+string replacingVariable(int[] b)
+{
+    string res = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        res += b[i] * -1 + " ";
+        
+    }
+    return res;
+}
+
+string A = replacingVariable(array);
+System.Console.Write(A + " ");
