ebcd5195490de6472a7008bc60ef7692983c9153
Oksana <79263143446@mail.ru>
2021-11-22 14:35:16 +0300

    task44

---
+++ task44/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+string BinaryNumber(int number)
+{
+
+    string binaryNum = string.Empty;
+
+    while (number > 0)
+    {
+        binaryNum += number % 2;
+        number = number / 2;
+
+    }
+    return binaryNum;
+}
+string result = BinaryNumber(27);
+System.Console.WriteLine(result);
