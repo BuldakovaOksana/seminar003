8843307bccb0aaef0d1be6099f93e5e1f9b80256
Oksana <79263143446@mail.ru>
2021-11-22 23:33:04 +0300

    task46

---
+++ task46/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать числа Фибоначчи
+
+
+void Fibonacci(int number)
+{
+    int A = 0;
+    int B = 1;
+    int C = 0;
+    for (int i = 0; i < number; i++)
+    {
+        C = A + B;
+        A = B;
+        B = C;
+        System.Console.WriteLine(A);
+    }
+
+}
+Fibonacci(10);
