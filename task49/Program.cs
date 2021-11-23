f4ff0f7d1a63b206a1b94b963e4a92e5f7a165a0
Oksana <79263143446@mail.ru>
2021-11-23 23:55:37 +0300

    task49

---
+++ task49/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+
+int[,] FillArray(int a, int b, int LeftBound, int RightBound)
+{
+    int[,] array = new int[a, b];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = new Random().Next(LeftBound, RightBound);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j]+ " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] array = FillArray(4, 4, 10, 100);
+PrintArray(array);
