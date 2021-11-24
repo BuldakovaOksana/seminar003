a67c572b4fcb0ee8e97a83f9eccdb6858b0a2e4c
Oksana <79263143446@mail.ru>
2021-11-24 16:29:48 +0300

    task52

---
+++ task52/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+
+
+int[,] FillArray(int row, int column)
+{
+    int[,]array=new int[row,column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j]=i+j;
+            
+        }
+    }
+    return array;
+}
+ 
+ void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}  
+int[,]array=FillArray(4,4);
+PrintArray(array);
+
+FillArray(4,4);
