71201aa652a7f980fccdacf2f706ee0c9eae024c
Oksana <79263143446@mail.ru>
2021-11-26 14:59:13 +0300

    seminar

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -1,81 +1,77 @@
 ﻿// ## Консольная игра "Угадай число" Комп загадывает число (в некотором отрезке), у игрока есть несколько попыток, чтобы угадать это число 
 // Игрок вводит различные числа, комп считает количество попыток, а также "говорит" введенное число больше или меньше загаданного. 
- 
-// 1. Нам необходимо задать исходные (обозначить условия). Отрезок (минимальное и максимальное значение), количество попыток. 
-// Один способ - задать как константы, второй способ - запросить у игрока.
-//  2. Инициализировать игру. Загадываем число и приветсвуем игрока, объясняем условия игры. 
-//  3. Делаем ход. Запрашиваем и получаем от игрока число. Сравниваем это число с загаданным, изменяем количество попыток и выводим результат. 
-//  4. Завершение игры (Если человек угадал, либо потратил все попытки). Либо предложить сыграть еще раз.
-// * GameInit() - метод, который задает все исходные значения в вашей игре. * MakeMove() - ходы, задаются этим методом. 
-// * RequestNumber() - запрос числа у игрока, обработка нештатных ситуаций. * CheckNumber(int PlayesrNumber, int SecretNumber) 
-// * GameOver(bool winner) -
 
 
-
-int LeftBound=1; 
-int RightBound=100;
-int attempts=7;
+int LeftBound = 1;
+int RightBound = 100;
+int attempts = 7;
 
 void GameInit(int LeftBound, int RightBound, int attempts)
 {
-   System.Console.WriteLine($"Привет! Давай поиграем в игру, Угадай число. Я загадаю число в отрезки от {LeftBound} до {RightBound}, а ты его угадаешь за {attempts} попыток.");
+    System.Console.WriteLine($"Привет! Давай поиграем в игру, Угадай число. Я загадаю число в отрезки от {LeftBound} до {RightBound}, а ты его угадаешь за {attempts} попыток.");
 }
 GameInit(LeftBound, RightBound, attempts);
 
 int MakeNumber(int LeftBound, int RightBound)
 {
-int number=new Random().Next(LeftBound, RightBound);
-return number;
+    int number = new Random().Next(LeftBound, RightBound);
+    return number;
 }
-int SecretNumber=MakeNumber(1,100);
-System.Console.WriteLine(SecretNumber);
+int SecretNumber = MakeNumber(1, 100);
 
-System.Console.WriteLine("Какое число я загадал? Ввуди число: ");
 
 int EnterNumber()
-{ int PlayesrNumber=0;
-    return  PlayesrNumber= int.Parse(Console.ReadLine()); 
+{
+    int PlayesrNumber = 0;
+    return PlayesrNumber = int.Parse(Console.ReadLine());
 }
-int PlayesrNumber=EnterNumber();
+
 
 int MakeMove(int PlayesrNumber, int SecretNumber, int attempts)
 {
-int count=attempts;
-while (true)
-{
-     
 
 
-    if (PlayesrNumber==SecretNumber)
-    {
-        System.Console.WriteLine("Вы выйграли!!!");
-    }
-    else
-    {
-        
-    
-    if (PlayesrNumber>SecretNumber)
+    if (PlayesrNumber > SecretNumber)
     {
         System.Console.WriteLine("Ваше число больше загаданного");
+
     }
-    if (PlayesrNumber<SecretNumber)
+    if (PlayesrNumber < SecretNumber)
     {
         System.Console.WriteLine("Ваше число меньше загаданного");
+
     }
-    }
+    attempts--;
+    return attempts;
 
 }
 
 
-// int GoThroughCycle(int attempts)
-// {
-// while (attempts>0)
-// {
-//     MakeMove(PlayesrNumber, SecretNumber, attempts);
-//     attempts--;
-// }
+int GoThroughCycle(int attempts)
+{
+    while (attempts > 0)
+    {
+        System.Console.WriteLine("Какое число я загадал? Введи число: ");
+        int PlayesrNumber = EnterNumber();
+        if (PlayesrNumber == SecretNumber)
+        {
+            System.Console.WriteLine("Вы выиграли!!!");
 
-// return attempts;
-// }
-// int StillAttempts=GoThroughCycle(attempts);
- System.Console.WriteLine($"у вас осталось {StillAttempts}");
+            return attempts;
+        }
+        int move = MakeMove(PlayesrNumber, SecretNumber, attempts);
+        attempts--;
+        System.Console.WriteLine($"У вас осталось {attempts} попыток ");
+
+        if (attempts <= 0)
+        {
+            System.Console.WriteLine("Вы проиграли!");
+        }
+    }
+
+
+    return attempts;
+}
+GoThroughCycle(attempts);
+
+System.Console.WriteLine($"Я загадал число {SecretNumber}");
