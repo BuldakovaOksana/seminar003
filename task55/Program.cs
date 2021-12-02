44fdfb3d2064a3520608010ad0d9c2b83ea41ed6
Oksana <79263143446@mail.ru>
2021-12-02 12:30:44 +0300

    task55

---
+++ task55/Program.cs
@@ -0,0 +1,42 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+
+int[,] GetArray(int row, int column, int leftBound, int rightBound)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(leftBound, rightBound);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] FillArray=GetArray(5,5,1,10);
+PrintArray(FillArray);
+
+int SumDiagonals(int[,] array)
+{
+    int sum=0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum+=array[i,i];
+    }
+    return sum;
+}
+System.Console.WriteLine(SumDiagonals(FillArray));
