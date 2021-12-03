d5e3ffbd10ac6a496035267c6111b300f69dad4d
Oksana <79263143446@mail.ru>
2021-12-03 11:36:36 +0300

    task56

---
+++ task56/Program.cs
@@ -0,0 +1,55 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+
+int[,] FillArray(int row, int column, int leftBound, int rightBound)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(leftBound, rightBound);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] getArray = FillArray(4, 4, 1, 10);
+PrintArray(getArray);
+
+void ArithmeticMeanColumn(int[,] array)
+{
+    double[] arr = new double[array.GetLength(1)];
+    //double sum=0;
+    
+    //double arithmeticMean = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+
+            // System.Console.Write(sum + " ");
+            //arithmeticMean = sum / 5;
+            //arr[j] = arithmeticMean;
+        }
+        
+      System.Console.Write($"{arr[j] = sum / array.GetLength(0)} ");
+    }
+    
+}    
+ArithmeticMeanColumn(getArray);
