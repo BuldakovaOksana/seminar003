8cfb12c15c9de33af987c19e2e25cf781c654bf0
Oksana <79263143446@mail.ru>
2022-01-14 16:24:28 +0300

    add task 45 47 58 59

---
+++ task58/Program.cs
@@ -0,0 +1,56 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+
+int[,] FillArray(int row, int column, int leftBound, int rightBound)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(leftBound, rightBound);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] GetArray = FillArray(4, 6, 1, 10);
+PrintArray(GetArray);
+
+
+int[,] ArrangeRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                int max = j;
+                if (array[i, k] > array[i, max])
+                {
+                    max = k;
+                    int temp = array[i, j];
+                    array[i, j] = array[i, max];
+                    array[i, max] = temp;
+
+                }
+            }
+        }
+    }
+    return array;
+}
+int[,] result = ArrangeRow(GetArray);
+System.Console.WriteLine();
+PrintArray(result);
---
+++ task59/Program.cs
@@ -0,0 +1,50 @@
+﻿// Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, 
+// что это невозможно (в случае, если матрица не квадратная).
+
+
+int [,] FillArray (int row, int column, int leftBound, int rightBound)
+{
+    int [,] array= new int [ row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i,j]= new Random().Next(leftBound, rightBound);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray( int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array [i,j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int [,] Array= FillArray(5, 5, 1, 10);
+PrintArray(Array);
+System.Console.WriteLine();
+
+int [,] ChengeRowOnColumn(int [,] array)
+{ //int j = 0;
+    for (int i = 0;  i < array.GetLength(0); i++)
+    {
+          for (int j = i; j < array.GetLength(1); j++)
+          {
+            int temp=array[i,j];
+          array[i,j]=array[j,i];
+          array[j,i]=temp;
+          
+         }
+    }
+    return array;
+}
+int [,] ChengeArray=ChengeRowOnColumn(Array);
+PrintArray(ChengeArray);
